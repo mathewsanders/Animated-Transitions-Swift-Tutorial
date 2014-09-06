a4854d85c186bf2f6811b364bbb3865018ac7abc
Mathew Sanders <mathew.sanders@gmail.com>
2014-09-06 11:59:51 -0400

    Added protocols to TransitionManager
    TransitionManager has methods to adhere to
    UIViewControllerAnimatedTransitioning and
    UIViewControllerTransitioningDelegate but we still need to do
    animateTransition

--- Transition/TransitionManager.swift
+++ Transition/TransitionManager.swift
@@ -8,6 +8,31 @@
 
 import UIKit
 
-class TransitionManager: NSObject {
-   
+class TransitionManager: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate  {
+ 
+    // MARK: UIViewControllerAnimatedTransitioning protocol methods
+    
+    // animate a change from one viewcontroller to another
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        // TODO: Perform the animation
+    }
+    
+    // return how many seconds the transiton animation will take
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning) -> NSTimeInterval {
+        return 0.5
+    }
+    
+    // MARK: UIViewControllerTransitioningDelegate protocol methods
+    
+    // return the animataor when presenting a viewcontroller
+    // remmeber that an animator (or animation controller) is any object that aheres to the UIViewControllerAnimatedTransitioning protocol
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+    
+    // return the animator used when dismissing from a viewcontroller
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return self
+    }
+    
 }
