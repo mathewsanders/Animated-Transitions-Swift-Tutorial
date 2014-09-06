d51a4a636868a7576762c7246ba174e4ec06c577
Mathew Sanders <mathew.sanders@gmail.com>
2014-09-06 11:54:10 -0400

    Crated TransitionManager class
    The TransitionManager class will act as animator between two screens,
    and as a delegate to tell which animator to use for presenting and
    dismissing a screen

---
+++ Transition/TransitionManager.swift
@@ -0,0 +1,13 @@
+//
+//  TransitionManager.swift
+//  Transition
+//
+//  Created by Mathew Sanders on 9/6/14.
+//  Copyright (c) 2014 Mat. All rights reserved.
+//
+
+import UIKit
+
+class TransitionManager: NSObject {
+   
+}
