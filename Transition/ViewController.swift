d3bf34418cf7622beb3d82715c8ad5356983dd06
Mathew Sanders <mathew.sanders@gmail.com>
2014-09-06 11:52:29 -0400

    Added exit segue

--- Transition/ViewController.swift
+++ Transition/ViewController.swift
@@ -19,7 +19,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func unwindToViewController(segue: UIStoryboardSegue) {
+        // empty for now
+    }
 
 }
 
