47b6667bf84d9354e96cedfa23df7e36fd42d309
Mathew Sanders <mathew.sanders@gmail.com>
2014-09-06 12:09:48 -0400

    Add slide transition animation

--- Transition/TransitionManager.swift
+++ Transition/TransitionManager.swift
@@ -14,7 +14,44 @@ class TransitionManager: NSObject, UIViewControllerAnimatedTransitioning, UIView
     
     // animate a change from one viewcontroller to another
     func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
-        // TODO: Perform the animation
+        
+        // get reference to our fromView, toView and the container view that we should perform the transition in
+        let container = transitionContext.containerView()
+        let fromView = transitionContext.viewForKey(UITransitionContextFromViewKey)!
+        let toView = transitionContext.viewForKey(UITransitionContextToViewKey)!
+        
+        // set up from 2D transforms that we'll use in the animation
+        let offScreenRight = CGAffineTransformMakeTranslation(container.frame.width, 0)
+        let offScreenLeft = CGAffineTransformMakeTranslation(-container.frame.width, 0)
+        
+        // start the toView to the right of the screen
+        toView.transform = offScreenRight
+        
+        // add the both views to our view controller
+        container.addSubview(toView)
+        container.addSubview(fromView)
+        
+        // get the duration of the animation
+        // DON'T just type '0.5s' -- the reason why won't make sense until the next post
+        // but for now it's important to just follow this approach
+        let duration = self.transitionDuration(transitionContext)
+        
+        // perform the animation!
+        // for this example, just slid both fromView and toView to the left at the same time
+        // meaning fromView is pushed off the screen and toView slides into view
+        // we also use the block animation usingSpringWithDamping for a little bounce
+        UIView.animateWithDuration(duration, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.8, options: nil, animations: {
+            
+            fromView.transform = offScreenLeft
+            toView.transform = CGAffineTransformIdentity
+            
+            }, completion: { finished in
+                
+                // tell our transitionContext object that we've finished animating
+                transitionContext.completeTransition(true)
+                
+        })
+        
     }
     
     // return how many seconds the transiton animation will take
--- Transition/ViewController.swift
+++ Transition/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    let transitionManager = TransitionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,17 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    
+        // this gets a reference to the screen that we're about to transition to
+        let toViewController = segue.destinationViewController as UIViewController
+        
+        // instead of using the default transition animation, we'll ask
+        // the segue to use our custom TransitionManager object to manage the transition animation
+        toViewController.transitioningDelegate = self.transitionManager
+        
     }
     
     @IBAction func unwindToViewController(segue: UIStoryboardSegue) {
