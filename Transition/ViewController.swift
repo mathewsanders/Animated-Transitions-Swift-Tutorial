a61e02e98dc00ee2ae559523711ae6079e29231f
Mathew Sanders <mathew.sanders@gmail.com>
2014-09-06 11:50:33 -0400

    First commit
    Layout screens in storyboard

---
+++ Transition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Transition
+//
+//  Created by Mathew Sanders on 9/6/14.
+//  Copyright (c) 2014 Mat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
